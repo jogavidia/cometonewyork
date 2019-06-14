15d38585ec171cb94d079bcb9d39f9f34d430b50
Joga <joga.gavidia@gmail.com>
2019-06-13 21:45:32 -0400

    Updates

---
+++ precache-manifest.b879e791aa10ee4f72ee7f8078783515.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0909263d5f5d43eaf94cece87b1ee92",
+    "url": "/cometonewyork/index.html"
+  },
+  {
+    "revision": "7894146cb01e3aa630ef",
+    "url": "/cometonewyork/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/cometonewyork/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "7894146cb01e3aa630ef",
+    "url": "/cometonewyork/static/js/main.bdad56c5.chunk.js"
+  },
+  {
+    "revision": "52802481c98991dfc77d",
+    "url": "/cometonewyork/static/js/runtime~main.337cd8fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cometonewyork/static/media/logo.5d5d9eef.svg"
+  }
+]);
